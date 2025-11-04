270030c90f9cfa4729596aa5a3166f3e63bac0e3
elmoprimmer <elmo.primmer@gmail.com>
2025-11-04 15:55:17 +0100

    more gitignore

--- losses/.ipynb_checkpoints/sparcity-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-from torch.nn import functional as F
-
-def l2_sparsity(masks):
-    reg = 0.0
-    for M in masks:
-        reg = reg + (M ** 2).sum()
-    return reg
